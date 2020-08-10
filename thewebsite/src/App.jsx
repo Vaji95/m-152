f3a10faa73cd3d8ad370dd2b0fbb5781788a71de
Vajeeran Logeswaran <vajeeran.logeswaran@advis.ch>
2020-08-10 11:20:53 +0200

    create Header and Menu

---
+++ thewebsite/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Menu from "./Components/Menu";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
